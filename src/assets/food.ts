5e5bc1596c28c746091f08ee04f7d169a4258f0b
Josa <pratamajosa1234@gmail.com>
2024-10-06 22:32:53 +0700

    feat: add images for seeder assets

---
+++ src/assets/food.ts
@@ -0,0 +1,64 @@
+export const ImageFoods = {
+  ayamGoreng: "/images/foods/ayam-goreng.jpg",
+  ikanSalmon: "/images/foods/ikan-salmon.jpeg",
+  kentangRebus: "/images/foods/kentang-rebus.jpg",
+  nasiPutih: "/images/foods/nasi-putih.webp",
+  pasta: "/images/foods/pasta.jpg",
+  quinoa: "/images/foods/quinoa.jpeg",
+  rotiGandung: "/images/foods/roti-gandum.jpg",
+  sayurBayam: "/images/foods/sayur-bayam.jpg",
+  tahu: "/images/foods/tahu.webp",
+  tempe: "/images/foods/tempe.jpg",
+};
+
+export const ImageDrinks = {
+  airMineral: "/images/drinks/air-mineral.jpg",
+  anggurMerah: "/images/drinks/anggur-merah.webp",
+  bir: "/images/drinks/bir.jpeg",
+  jusApel: "/images/drinks/jus-apel.png",
+  jusJeruk: "/images/drinks/jus-jeruk.jpg",
+  kopiHitam: "/images/drinks/kopi-hitam.jpeg",
+  smoothieBuah: "/images/drinks/smoothie-buah.png",
+  soda: "/images/drinks/soda.jpg",
+  susu: "/images/drinks/susu.webp",
+  tehManis: "/images/drinks/teh-manis.png",
+};
+
+export const ImageFruits = {
+  apel: "/images/fruits/apel.webp",
+  buahPir: "/images/fruits/buah-pir.jpeg",
+  delima: "/images/fruits/delima.webp",
+  jeruk: "/images/fruits/jeruk.jpeg",
+  kiwi: "/images/fruits/kiwi.jpeg",
+  mangga: "/images/fruits/mangga.jpg",
+  nanas: "/images/fruits/nanas.jpg",
+  pisang: "/images/fruits/pisang.jpg",
+  semangka: "/images/fruits/semangka.jpeg",
+  strawberry: "/images/fruits/strawberry.jpeg",
+};
+
+export const ImageSnacks = {
+  biskuit: "/images/snacks/biskuit.jpeg",
+  chips: "/images/snacks/chips.webp",
+  coklat: "/images/snacks/coklat.png",
+  granolaBar: "/images/snacks/granola-bar.jpg",
+  kacangPanggang: "/images/snacks/kacang-panggang.jpg",
+  kacangPolong: "/images/snacks/kacang-polong.jpeg",
+  keripikKentang: "/images/snacks/keripik-kentang.jpeg",
+  kueKering: "/images/snacks/kue-kering.jpeg",
+  pisangGoreng: "/images/snacks/pisang-goreng.jpeg",
+  popcorn: "/images/snacks/popcorn.jpeg",
+};
+
+export const ImageVegetables = {
+  bayam: "/images/vegetables/bayam.jpeg",
+  brokoli: "/images/vegetables/brokoli.jpeg",
+  kacangHijau: "/images/vegetables/kacang-hijau.jpeg",
+  kacangPanjang: "/images/vegetables/kacang-panjang.jpg",
+  kailan: "/images/vegetables/kailan.jpg",
+  kembangKol: "/images/vegetables/kembang-kol.jpg",
+  lobak: "/images/vegetables/lobak.jpg",
+  tomat: "/images/vegetables/tomat.jpeg",
+  wortel: "/images/vegetables/wortel.jpg",
+  zucchini: "/images/vegetables/zucchini.jpg",
+};
