6b2256d41fbfb4c63d086f0374c9e921ec7787fc
Josa <pratamajosa1234@gmail.com>
2024-10-06 22:56:21 +0700

    feat: add languange features

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    // reactStrictMode: true,
+    locales: ["en", "id"],
+    defaultLocale: "en",
+  },
+};
+
+module.exports = nextConfig;
